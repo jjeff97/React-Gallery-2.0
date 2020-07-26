67f52b086b3bdccda4ba467b0af846ae8c7e1470
James Jeffeson <jrjefferson830@gmail.com>
2020-07-26 17:07:59 -0500

    installing BS mod

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,34 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: "images/goat_small.jpg",
+    description: "Photo of a goat taken at Glacier National Park.",
+    likes: 0,
+  },
+  {
+    id: 2,
+    path: "images/BlackPanther.jpg",
+    description: "Black Jaguar.",
+    likes: 0,
+  },
+  {
+    id: 3,
+    path: "images/CompKeyboard.jpg",
+    description: "Computer Keyboard.",
+    likes: 0,
+  },
+  {
+    id: 4,
+    path: "images/Keyboard.jpg",
+    description: "Music Keyboard.",
+    likes: 0,
+  },
+  {
+    id: 5,
+    path: "images/MexicanSalad.jpg",
+    description: "Mexican Salad.",
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
